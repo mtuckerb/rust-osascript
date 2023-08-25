da5f5f5c7603ed9a0a7c1337dd4056b44b9a7bfe
M. Tucker Bradford <tucker@tuckerbradford.com>
2023-08-25 14:20:00 -0400

    adds AppleScript

--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,3 @@
-//! This library implements limited functionality for the OSA System on macOS.
-//! In particular it allows you to execute JavaScript via the OSA system to
-//! script applications.  It's particularly useful if you need to tell other
-//! applications to execute certain functionality.
-//!
-//! Currently only JavaScript is supported.  Parameters passed to it show up
-//! as `$params` and the return value from the script (as returned with the
-//! `return` keyword) is deserialized later.
-//!
-//! # Example
-//!
-//! ```
-//! extern crate osascript;
-//! #[macro_use] extern crate serde_derive;
-//! 
-//! #[derive(Serialize)]
-//! struct AlertParams {
-//!     title: String,
-//!     message: String,
-//!     alert_type: String,
-//!     buttons: Vec<String>,
-//! }
-//! 
-//! #[derive(Deserialize)]
-//! struct AlertResult {
-//!     #[serde(rename="buttonReturned")]
-//!     button: String,
-//! }
-//! 
-//! fn main() {
-//!     let script = osascript::JavaScript::new("
-//!         var App = Application('Finder');
-//!         App.includeStandardAdditions = true;
-//!         return App.displayAlert($params.title, {
-//!             message: $params.message,
-//!             'as': $params.alert_type,
-//!             buttons: $params.buttons,
-//!         });
-//!     ");
-//! 
-//!     let rv: AlertResult = script.execute_with_params(AlertParams {
-//!         title: "Shit is on fire!".into(),
-//!         message: "What is happening".into(),
-//!         alert_type: "critical".into(),
-//!         buttons: vec![
-//!             "Show details".into(),
-//!             "Ignore".into(),
-//!         ]
-//!     }).unwrap();
-//! 
-//!     println!("You clicked '{}'", rv.button);
-//! }
-//! ```
 use std::process;
 use std::io;
 use std::fmt;
@@ -127,6 +74,8 @@ fn wrap_code<S: Serialize>(code: &str, params: S) -> Result<String, Error> {
     Ok(String::from_utf8(buf)?)
 }
 
+//----------------------
+/// Holds an apple flavoured JavaScript
 impl JavaScript {
     /// Creates a new script from the given code.
     pub fn new(code: &str) -> JavaScript {
@@ -142,12 +91,48 @@ impl JavaScript {
 
     /// Executes the script and passes the provided arguments.
     pub fn execute_with_params<'a, S: Serialize, D: DeserializeOwned>(&self, params: S)
-        -> Result<D, Error>
+    -> Result<D, Error>
     {
         let wrapped_code = wrap_code(&self.code, params)?;
         let output = process::Command::new("osascript")
             .arg("-l")
             .arg("JavaScript")
+            .arg("-e")
+            .arg(&wrapped_code)
+            .output()?;
+        if output.status.success() {
+            Ok(serde_json::from_slice(&output.stdout)?)
+        } else {
+            Err(Error::Script(String::from_utf8(output.stderr)?))
+        }
+    }
+}
+
+pub struct AppleScript {
+    code: String,
+}
+
+impl AppleScript {
+    /// Creates a new script from the given code.
+    pub fn new(code: &str) -> JavaScript {
+        JavaScript {
+            code: code.to_string(),
+        }
+    }
+
+    /// Executes the script and does not pass any arguments.
+    pub fn execute<'a, D: DeserializeOwned>(&self) -> Result<D, Error> {
+        self.execute_with_params(EmptyParams {})
+    }
+
+    /// Executes the script and passes the provided arguments.
+    pub fn execute_with_params<'a, S: Serialize, D: DeserializeOwned>(&self, params: S)
+    -> Result<D, Error>
+    {
+        let wrapped_code = wrap_code(&self.code, params)?;
+        let output = process::Command::new("osascript")
+            .arg("-l")
+            .arg("AppleScript")
             .arg("-e")
             .arg(&wrapped_code)
             .output()?;
